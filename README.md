a59025d748f890fb18d8925ea439466a0f3c29ce
weifengming <15611784834@163.com>
2020-02-01 12:55:26 +0800

    Ribbon负载均衡自定义

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 
 服务提供者：microservicecloud-provider-dept-8001端口：8001
 
+测试Ribbon负载均衡：microservicecloud-provider-dept-8002端口：8002，microservicecloud-provider-dept-8003端口：8003
+
 服务消费者：microservicecloud-consumer-dept-80端口：80
 
 Eureka注册中心：microservicecloud-eureka-7001端口：7001
 
 Eureka集群：microservicecloud-eureka-7002端口：7002，microservicecloud-eureka-7003端口：7003
+
+microservicecloud-consumer-dept-80中新增ribbon自定义，主启动类中注释了负载规则
--- microservicecloud-consumer-dept-80/src/main/java/com/wfm/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/wfm/springcloud/DeptConsumer80_App.java
@@ -6,9 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MyRoundRobinRule.class)//自定义ribbon定义算法
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeptConsumer80_App.class,args);
+        SpringApplication.run(DeptConsumer80_App.class, args);
     }
 }
